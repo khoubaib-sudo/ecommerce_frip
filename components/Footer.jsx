436d6d706918b10465bfe044f5a64c5aa65b7657
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-01-23 11:00:26 +0100

    updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Link , Button} from 'next/link';
 import { AiFillInstagram, AiFillFacebook} from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <p className="icons">
-        <AiFillInstagram />
-        <AiFillFacebook />
+        <a target="_blank" href="https://instagram.com/">
+          <AiFillInstagram />
+        </a>
+        <a target="_blank" href="https://facebook.com/">
+          <AiFillFacebook />
+        </a>
       </p>
     </div>
   )
